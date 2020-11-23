3403b6b4007c863dd2a40c24c540d9700133bcf2
Chi-Hsun <73160526+Chi-Hsun@users.noreply.github.com>
2020-11-24 00:30:08 +0800

    Create exhausted search.py

---
+++ exhausted search.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+filename = 'substrate1123.txt'
+
+def read_substrate(filename):
+    with open(filename, 'r') as f:#with語句自動呼叫close()方法
+        Inp1_nodenum = int(f.readline())
+        Inp2_nodenum = int(f.readline())
+        link = np.zeros((Inp1_nodenum+Inp2_nodenum,Inp1_nodenum+Inp2_nodenum))
+        CPU = np.zeros(Inp1_nodenum+Inp2_nodenum)
+        
+        for i in range(2):
+            line = f.readline()
+            temp = line.split()
+            for j in range(len(temp)):
+                CPU[i*len(temp)+j] = int(temp[j])
+        for i in range(Inp1_nodenum+Inp2_nodenum):
+            line = f.readline()
+            link[i] = line.split()
+            
+    return CPU,link
+
+cpu, link = read_substrate(filename)
+
+
+filename = 'virtual1123.txt'
+def read_virtual(filename):
+    VNR_list = np.genfromtxt(filename)
+    return VNR_list
+VNR_list = read_virtual(filename)
+print(VNR_list)
