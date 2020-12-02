a9cb8e3c61ff1acb02720ecd02ee86b752a1061f
Chi-Hsun <73160526+Chi-Hsun@users.noreply.github.com>
2020-12-03 03:26:38 +0800

    Update exhausted search.py

--- exhausted search.py
+++ exhausted search.py
@@ -1,7 +1,4 @@
-#import VNE_func as fc
-import itertools
-import numpy as np
-
+from copy import copy, deepcopy
 filename_sub = 'substrate1130.txt'
 filename_vir = 'virtual1130.txt'
 cpu_substrate, bw_substrate = read_substrate(filename_sub)
@@ -64,19 +61,20 @@ for time in range(num_of_timeslots):
             #print("VNR_list: ",VNR_list)
 
             subnodes = list(range(0,len(cpu_substrate)))
-            virlinks = list(range(0,len(node_cap)-1))
+            #virlinks = list(range(0,len(node_cap)-1))
 
 
             # test all permutations
-            all_combinations = list(itertools.permutations(subnodes))
-            all_link_combinations = list(itertools.permutations(virlinks)) #列出所有找link的順序
-            #print("all_link_combinations = ", all_link_combinations)
+            all_combinations = list(itertools.permutations(subnodes, node_num))
             #print(all_combinations)
             opt_cost = sum(bw_cap)
             temp_best_cost = 100000000
             temp_opt = -1  #node是第幾個組合最好
-            opt_link = -1  #link是第幾個組合最好
+            opt_link_index = -1  #link是第幾個組合最好
+            #print("cpu substrate = ", cpu_substrate)
+            #print("bw_substrate = ", bw_substrate)
             for i in range(len(all_combinations)):
+                #print("i = ",i)
                 cost = 100000000
                 node_check = 1
                 bw_check = 1
@@ -86,35 +84,76 @@ for time in range(num_of_timeslots):
                         node_check = 0
                         break
                        
-                if node_check==1:   #node測試過再測試link                    
-                    link_cost = np.zeros(len(all_link_combinations))
-                    for x in range(len(all_link_combinations)):                        
-                        copy_bw_substrate = bw_substrate
-                        for s in range(len(node_cap)-1):
-                            path = shortest_path(all_combinations[i][all_link_combinations[x][s]], all_combinations[i][all_link_combinations[x][s]+1], bw_cap[all_link_combinations[x][s]], copy_bw_substrate)
-                            #print("path = ",path)
-                            if path == -1:
-                                link_cost[x] = 100000000
+                if node_check==1:   #node測試過再測試link         
+                    #print("pass node check, for the ",i, "combinations")
+                    bw_substrate_copy = deepcopy(bw_substrate)
+                    graph = build_graph(bw_substrate_copy,1)
+                    path_comb = []
+                    for s in range(len(bw_cap)):
+                        path = depthFirst(graph, all_combinations[i][s], [], [])
+                        path = find_all_path(all_combinations[i][s], all_combinations[i][s+1], path)  #篩選出從source到destination的所有路境
+                        path_comb.append(path)
+
+                    #print("path_comb = ", path_comb)
+                    total_path_comb = []
+                    for s in range(len(bw_cap)):
+                        each_path_num = list(range(0,len(path_comb[s])))
+                        total_path_comb.append(each_path_num)
+                    #print("total path comb = ", total_path_comb)                    
+                    for k in range(len(bw_cap)):
+                        if total_path_comb[k]==[]:
+                            #print("link mapping fail!")
+                            bw_check = 0
+                            break
+                if node_check==1 and bw_check==1:
+                    all_comb = [[]]
+                    possible_path_comb = list_all_comb(total_path_comb, all_comb)
+                    #print("possible path comb = ", possible_path_comb)
+                    
+                    # check whether each path candidate satisfies bw constraints, if yes, calculate the cost and compare with temp_best_cost.
+                    for s in range(len(possible_path_comb)):
+                        cost = 0
+                        bw_copy = deepcopy(bw_substrate)
+                        #print("bw_substrate_copy = ", bw_copy)
+                        #print("turn to the ,", s, "possible_path_comb") 
+                        flag = 0
+                        for k in range(len(bw_cap)):
+                            if flag ==-1:
                                 break
                             else:
-                                for j in range(len(path)-1):
-                                    copy_bw_substrate[path[j]][path[j+1]] = copy_bw_substrate[path[j]][path[j+1]] - bw_cap[all_link_combinations[x][s]]  #扣掉頻寬資源
-                                    copy_bw_substrate[path[j+1]][path[j]] = copy_bw_substrate[path[j]][path[j+1]]
-                                    hop = len(path)-1
-                                    link_cost[x] = link_cost[x] + bw_cap[s]*hop
-                                #print("link_cost", x, "=", link_cost[x])
-                #print("link cost = ", link_cost)
-                #print("min link cost")
-                if node_check==1 and min(link_cost)<temp_best_cost:
-                    opt_link = all_link_combinations[list(link_cost).index(min(link_cost))]
-                    temp_best_cost = min(link_cost)
-                    temp_opt = i
-                    if temp_best_cost == opt_cost:   #找到最佳解就停止
-                        #print("Find the opt solution ", i)
-                        break
-            #print("temp opt = ", temp_opt)
-            #print("opt link = ", opt_link)
-            #print("the solution is ", all_combinations[temp_opt])
+                                current_path = path_comb[k][possible_path_comb[s][k]]
+                                #print("path for virtual link ",k, "is ", current_path)
+                                for h in range(len(current_path)-1):
+                                    bw_copy[current_path[h]][current_path[h+1]] = bw_copy[current_path[h]][current_path[h+1]] - bw_cap[k]
+                                    bw_copy[current_path[h+1]][current_path[h]] = bw_copy[current_path[h]][current_path[h+1]]
+                                    if bw_copy[current_path[h]][current_path[h+1]]>=0:
+                                        cost = cost + bw_cap[k]
+                                        #print("bw_copy = ",bw_copy)
+                                        #print("cost = ", cost)
+                                    else:
+                                        cost = 100000000
+                                        #print("link mapping fail!")
+                                        flag = -1
+                                        break
+                        if cost<temp_best_cost:
+                            #print("update current best!")
+                            temp_best_cost = cost
+                            temp_opt = i
+                            opt_link_index = s   
+                            link_opt = []
+                            for x in range(len(bw_cap)):
+                                link_opt.append(path_comb[x][possible_path_comb[opt_link_index][x]])
+                            #print("current link opt = ", link_opt)
+                                                                
+                if temp_best_cost == opt_cost:   #找到最佳解就停止
+                    #print("Find the opt solution ", i, ", and the best path is ", link_opt )
+                    break
+            if temp_opt!=-1 and opt_link_index!=-1:
+                #print("temp opt = ", temp_opt)
+                #print("link opt = ", link_opt)
+                print("the solution is ", all_combinations[temp_opt])
+            else:
+                print("reject this VNR!")
 
             if temp_opt!=-1:
                 acceptance = acceptance+1
@@ -127,23 +166,20 @@ for time in range(num_of_timeslots):
                     node_resource_table[all_combinations[temp_opt][i]].append(node_cap[i])
 
             
-                #紀錄node資源什麼時候可以釋出，並且扣掉node資源
+                #紀錄link資源什麼時候可以釋出，並且扣掉link資源
                 for i in range(len(node_cap)-1):
-                    path = shortest_path(all_combinations[temp_opt][opt_link[i]], all_combinations[temp_opt][opt_link[i]+1], bw_cap[opt_link[i]], bw_substrate)
-                                                   
-                
-               
-                    for j in range(len(path)-1):
-                        bw_substrate[path[j]][path[j+1]] = bw_substrate[path[j]][path[j+1]] - bw_cap[opt_link[i]] #扣掉頻寬資源
-                        bw_substrate[path[j+1]][path[j]] = bw_substrate[path[j]][path[j+1]]
-                        a = path[j]
-                        b = path[j+1]
+                    
+                    for j in range(len(link_opt[i])-1):
+                        bw_substrate[link_opt[i][j]][link_opt[i][j+1]] = bw_substrate[link_opt[i][j]][link_opt[i][j+1]] - bw_cap[i] #扣掉頻寬資源
+                        bw_substrate[link_opt[i][j+1]][link_opt[i][j]] = bw_substrate[link_opt[i][j]][link_opt[i][j+1]]
+                        a = link_opt[i][j]
+                        b = link_opt[i][j+1]
                         if a<b:
                             bw_resource_table[link_pairs.index([a,b])].append(arr_time+life_time)
-                            bw_resource_table[link_pairs.index([a,b])].append(bw_cap[opt_link[i]])
+                            bw_resource_table[link_pairs.index([a,b])].append(bw_cap[i])
                         else:
                             bw_resource_table[link_pairs.index([b,a])].append(arr_time+life_time)
-                            bw_resource_table[link_pairs.index([b,a])].append(bw_cap[opt_link[i]])
+                            bw_resource_table[link_pairs.index([b,a])].append(bw_cap[i])
             if VNR_list!=[]:
                 arr_time = VNR_list[0]  
             else:
